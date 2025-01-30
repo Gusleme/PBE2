74547297cf4d0bbdd1ef742c34b139326b50d7f6
Gusleme <159180396+Gusleme@users.noreply.github.com>
2025-01-30 12:48:31 -0300

    Add files via upload

---
+++ prjloja/src/main/java/com/example/demo/PrjlojaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjlojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjlojaApplication.class, args);
+	}
+
+}
